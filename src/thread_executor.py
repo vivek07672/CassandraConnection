83fd72e8653f7d6076a2d910ee9b1eea9803bac4
Vivek Gupta <vivek@localhost.localdomain>
2015-02-04 16:29:21 +0530

    removing redundant code

--- src/thread_executor.py
+++ src/thread_executor.py
@@ -7,30 +7,7 @@ import threading
 
 from cluster_handler import DatabaseHandler
 
-
-CONDITION_VAR = threading.Condition()
-CHECK_IN_DATE = None
-VOYAGER_CITY_CODE = None
-HOTEL_CODE = None
-PACKAGE = None
-
 db_handler = DatabaseHandler()
-
-class Reader(threading.Thread):
-    def __init__(self, threadID):
-        threading.Thread.__init__(self)
-        self.type = 'reader'
-        self.thread_id = threadID
-    def run(self):
-        db_handler.read_random_data()
-            
-class Writer(threading.Thread):
-    def __init__(self,threadID):
-        threading.Thread.__init__(self)
-        self.type = 'writer'
-        self.thread_id = threadID
-    def run(self):
-        db_handler.write_info()
 
 while(1):
     print "press 1 to read random "
